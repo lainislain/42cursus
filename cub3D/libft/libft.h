22c71df2317b653219344a2d9b2a0ee91bd4d2c3
lainislain <maghat.abdessalam@gmail.com>
2021-04-26 02:55:24 +0000

    norming libft v3

--- cub3D/libft/libft.h
+++ cub3D/libft/libft.h
@@ -17,7 +17,7 @@
 # include <string.h>
 # include <stdlib.h>
 
-typedef	struct		s_list
+typedef	struct s_list
 {
 	void			*content;
 	struct s_list	*next;
@@ -36,7 +36,7 @@ size_t				ft_strlcat(char *dst, const char *src, size_t n);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *haystack,
-		const char *needle, size_t len);
+const char *needle, size_t len);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *str);
 int					ft_isalpha(int c);
@@ -67,5 +67,5 @@ void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-		void (*del)(void *));
+void (*del)(void *));
 #endif
