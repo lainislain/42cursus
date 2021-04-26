3fa40a3378e305ba34625de38527f1f61d839f75
lainislain <maghat.abdessalam@gmail.com>
2021-04-26 02:59:42 +0000

    norming libft v3

--- cub3D/libft/libft.h
+++ cub3D/libft/libft.h
@@ -35,7 +35,7 @@ size_t				ft_strlcpy(char *dst, const char *src, size_t n);
 size_t				ft_strlcat(char *dst, const char *src, size_t n);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
-char				*ft_strnstr(const char *haystack,const char *needle, \
+char				*ft_strnstr(const char *haystack, const char *needle, \
 size_t len);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *str);
