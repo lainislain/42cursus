a663cd94a5137ba40f44dd892a32a4b6ebeab372
root <root@localhost.localdomain>
2021-04-29 05:55:09 +0000

    exam training

--- exam_rank_02/union/union.c
+++ exam_rank_02/union/union.c
@@ -1,44 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   union.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amaghat <amaghat@student.1337.ma>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/14 12:10:40 by amaghat           #+#    #+#             */
-/*   Updated: 2021/02/25 15:34:49 by amaghat          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int
-	main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-	int	ascii[255];
-	int	i, j;
+  int ascii [255];
+  int   i;
+  int   j;
 
-	if (argc == 3)
-	{
-		i = 0;
-		while (i < 255)
-			ascii[i++] = 0;
-		i = 1;
-		while (i < 3)
-		{
-			j = 0;
-			while (argv[i][j])
-			{
-				if (!ascii[(int)argv[i][j]])
-				{
-					ascii[(int)argv[i][j]] = 1;
-					write(1, &argv[i][j], 1);
-				}
-				j++;
-			}
-			i++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+  if(argc == 3)
+  {
+    i = 0;
+    while(i < 255)
+      ascii[i++] = 0;
+    i = 1;
+    while(i < 3)
+    {
+      j = 0;
+      while(argv[i][j])
+      {
+        if(!ascii[(int)argv[i][j]])
+        {
+          ascii[(int)argv[i][j]] = 1;
+          write(1,&argv[i][j],1);
+        }
+        j++;
+      }
+      i++;
+    }
+  }
+  write(1,"\n",1);
+  return (0);
 }
